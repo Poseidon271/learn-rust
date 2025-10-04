b896fc493800682f5b9b932377e581a7f293d7e8
Poseidon271 <adityasisodia142@gmail.com>
2025-10-04 15:02:49 +0530

    ch2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, chuttad!");
+use std::io;
+
+fn main()
+{
+    println!("Guess the number");
+    println!("Please input your guess");
+    let mut num1 = String::new();
+   // let mut num1:u8;
+     io::stdin()
+     .read_line(&mut num1)
+     .expect("Failed to read line");
+     println!("correct :{num1}");
 }
