2a1df3b59be5c2ba946c818b8cc29b12e53941c3
Poseidon271 <adityasisodia142@gmail.com>
2026-02-02 01:18:55 +0530

    Reference

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
 fn main() {
-    
-    let s = String::from("hello");  // s comes into scope
+    let s1 = String::from("hello");
 
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
+    let len = calculate_length(&s1);
 
-    let x = 5;                      // x comes into scope
+    println!("The length of '{s1}' is {len}.");
+}
 
-    makes_copy(x);                  // Because i32 implements the Copy trait,
-                                    // x does NOT move into the function,
-                                    // so it's okay to use x afterward.
-
-} // Here, x goes out of scope, then s. However, because s's value was moved,
-  // nothing special happens.
-
-fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{some_string}");
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
-
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{some_integer}");
-} // Here, some_integer goes out of scope. Nothing special happens.
-
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
